c36aeb2084cb6adbba69cfd5bb6bca92feef27d5
Neto Russo <netoacrn1@gmail.com>
2024-05-02 14:31:44 -0300

    subindo aula do modulo 03

---
+++ src/componentes/Time/index.tsx
@@ -0,0 +1,33 @@
+import { IColaborador } from '../../compartilhado/interfaces/IColaborador'
+import Colaborador from '../Colaborador'
+import './Time.css'
+
+interface TimeProps {
+  corPrimaria: string
+  corSecundaria: string
+  nome: string
+  colaboradores: IColaborador[]
+}
+
+const Time = ({ corPrimaria, corSecundaria, nome, colaboradores } :TimeProps) => {
+  const css = { backgroundColor: corSecundaria }
+
+  return (
+    (colaboradores.length > 0) ? <section className='time' style={css}>
+      <h3 style={{ borderColor: corPrimaria }}>{nome}</h3>
+      <div className='colaboradores'>
+        {colaboradores.map(colaborador =>
+          <Colaborador
+            corDeFundo={corPrimaria}
+            key={colaborador.nome}
+            nome={colaborador.nome}
+            cargo={colaborador.cargo}
+            imagem={colaborador.imagem}
+          />)}
+      </div>
+    </section>
+      : ''
+  )
+}
+
+export default Time
